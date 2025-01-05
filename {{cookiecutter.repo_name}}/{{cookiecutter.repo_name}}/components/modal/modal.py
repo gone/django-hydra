207909965f940efda14bef29f51f9437e2ab627b
Ben Beecher <ben.beecher@hungryroot.com>
2025-01-05 13:51:01 -0500

    modals!

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/components/modal/modal.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/components/modal/modal.py
@@ -4,9 +4,7 @@ from django_components import Component, register
 class Modal(Component):
     template_name = "modal.html"
 
-    def get_context_data(self, button=None, width_class="max-w-xl", open=False):
+    def get_context_data(self, open=False):
         return {
-            "button": button,
-            "width_class": width_class,
             "open": open
         }
