54f32e7da1bc4f1518d694f92b4efebf82640b9d
Ben Beecher <ben.beecher@hungryroot.com>
2025-01-08 15:56:14 -0500

    fixing a few form bugs, and getting some styling fixed!

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/components/form/form.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/components/form/form.py
@@ -8,21 +8,19 @@ class Field(Component):
             raise RuntimeError(
                 f"Component '{self.name}' must be used with a field"
             )
-        if field.widget_type == "toggle":
-            return "form/toggle.html"
         elif field.widget_type == "checkbox":
             return "form/checkbox.html"
         elif field.widget_type in ["radioselect", "checkboxselectmultiple"]:
             return "form/radio.html"
-        elif "file" in field.widget_type:
-            return "form/file.html"
 
         return "form/input.html"
 
     def get_context_data(self, field, attrs=None):
         attrs = attrs or {}
         attrs['class'] = f"h-full {field.css_classes(extra_classes=attrs.get('class', ''))}".strip() #h-full overridden by user passed attrs
-
+        # checkbox is a little bit of a special snowflakex
+        if field.widget_type == "checkbox":
+            attrs['class'] += " relative flex items-start"
 
         return {
             'field': field,
