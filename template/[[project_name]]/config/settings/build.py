f743935a4d75030ebaa3f422ec8d953fc5b6439c
Ben Beecher <benbeecher@gmail.com>
2025-01-28 13:17:13 -0500

    changes from actually getting something on fly

--- template/[[project_name]]/config/settings/build.py
+++ template/[[project_name]]/config/settings/build.py
@@ -1,11 +1,18 @@
 # required settings for build command like collectstatic
 import os
-REQUIRED_KEYS = ['DJANGO_SECRET_KEY', "DATABASE_URL", "DJANGO_ALLOWED_HOSTS", "REDIS_URL",
-                 "DJANGO_AWS_ACCESS_KEY_ID", "DJANGO_AWS_SECRET_ACCESS_KEY",
-                 "DJANGO_AWS_STORAGE_BUCKET_NAME", "DJANGO_AWS_S3_ENDPOINT_URL"
-                 ]
+
+REQUIRED_KEYS = [
+    "SECRET_KEY",
+    "DATABASE_URL",
+    "DJANGO_ALLOWED_HOSTS",
+    "REDIS_URL",
+    "AWS_ACCESS_KEY_ID",
+    "AWS_SECRET_ACCESS_KEY",
+    "BUCKET_NAME",
+    "AWS_ENDPOINT_URL_S3",
+]
 for key in REQUIRED_KEYS:
-    os.environ[key] = 'dummy'
-os.environ['SENTRY_DSN'] = 'https://dummy@dummy.ingest.sentry.io/1234567'
+    os.environ[key] = "dummy"
+os.environ["SENTRY_DSN"] = "https://dummy@dummy.ingest.sentry.io/1234567"
 
 from .prod import *
